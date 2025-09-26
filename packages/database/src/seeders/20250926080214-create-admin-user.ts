8056f9c9475ec4d839f0eb5f0c638bc16874a4c6
Kasitinard <yelaysong15@gmail.com>
2025-09-26 15:59:34 +0700

    add seeder in dockerfile

--- packages/database/src/seeders/20250926080214-create-admin-user.ts
+++ packages/database/src/seeders/20250926080214-create-admin-user.ts
@@ -1,5 +1,4 @@
 import bcrypt from "bcryptjs";
-
 import { Migration } from 'sequelize-cli'
 
 export = {
@@ -7,22 +6,28 @@ export = {
     const now = new Date();
     const hashedPassword = await bcrypt.hash("140974saiaye", 10);
 
-    await queryInterface.bulkInsert("users", [
-      {
-        role_id: 1,
-        username: "admin",
-        email: "admin@gmail.com",
-        password: hashedPassword,
-        status: true,
-        recent_login: now,
-        last_login: now,
-        created_at: now,
-        updated_at: now,
-      },
-    ]);
+    const [user] = await queryInterface.sequelize.query(
+  `SELECT * FROM users WHERE email = 'admin@gmail.com'`
+);
+
+if (!user.length) {
+  await queryInterface.bulkInsert("users", [
+    {
+      role_id: 1,
+      username: "admin",
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      status: true,
+      recent_login: now,
+      last_login: now,
+      created_at: now,
+      updated_at: now,
+    }
+  ]);
+}
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete("users", { email: "admin@gmail.com" }, {});
   },
-} satisfies Migration
+} satisfies Migration;
